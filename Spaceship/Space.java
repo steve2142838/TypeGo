a1c3429f0b56455995d603ec6b10d42b6e3eb373
Hussainamin10 <hussain.amin@icloud.com>
2021-11-09 15:22:35 -0500

    Add rocket

--- Spaceship/Space.java
+++ Spaceship/Space.java
@@ -17,5 +17,15 @@ public class Space extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        rocket rocket = new rocket();
+        addObject(rocket,262,263);
     }
 }
