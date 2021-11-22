87a06f4621657353624e0b65fe49ff9ec61a5550
Hussainamin10 <hussain.amin@icloud.com>
2021-11-21 19:44:12 -0500

    Plane

---
+++ Spaceship/Plane.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Plane extends World
+{
+
+    /**
+     * Constructor for objects of class Plane.
+     */
+    public Plane()
+    {
+        super(600, 400, 1);
+    }
+}
